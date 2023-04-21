90280f54cf6775e818ea488b76cfbfa3ef9a473d
@vlehnhau <vlehnhau@students.uni-mainz.de>
2023-04-21 20:18:29 +0200

    second version

--- main.py
+++ main.py
@@ -1,51 +1,62 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QFormLayout, QWidget, QCheckBox, QPushButton, QSlider, QLabel, QMainWindow, QMenuBar, QStatusBar
-from PyQt6.QtCore import Qt, QTimer, QRect, QCoreApplication, QMetaObject
-from PyQt6.QtGui import QBrush, QColor, QPixmap
-
 import random
+import sys
 
 from PyQt6 import QtGui
+from PyQt6.QtCore import Qt, QTimer, QRect, QCoreApplication, QMetaObject
+from PyQt6.QtGui import QColor, QPixmap
+from PyQt6.QtWidgets import QApplication, QFormLayout, QWidget, QCheckBox, QPushButton, QSlider, QLabel, QMainWindow, \
+    QMenuBar, QStatusBar, QMenu, QMessageBox
 
 
 class Game(object):
     def __init__(self, hight: int, width: int):
-        self.player = Player(25, 25, 1)
+
+        # Create Player, add apple and define size of board
+        self.player = Player(round(width / 2), round(hight / 2), 1)
         self.currentApple = []
         self.x = hight
         self.y = width
 
-        self.border = True
-        self.speedUp = True
-        self.getFaster = False
+        self.border = True  # Setting: Boarder kills player or not (Default True)
+        self.speedUp = True  # Settign: Player gets faster after eating apple (Default _True)
 
+        self.getFaster = False  # Did the player just ate an apple?
+
+        # Spawn start apple
         self.apple = [0, 0]
         self.spawnApple()
 
+    # Spawns an apple
     def spawnApple(self):
         self.apple[0] = random.randint(1, self.x - 2)
         self.apple[1] = random.randint(1, self.y - 2)
 
+        # Take care that the field is not in use
         if self.apple in self.player.tail or self.apple == self.player.head:
             self.spawnApple()
 
+        # if the player uses Setting speed up tell the gui to refresh faster by setting getFaster on True
         if self.speedUp:
-            self.player.speed = self.player.speed + 10
-            self.getFaster = True
+            if self.player.speed <= 110:
+                self.player.speed = self.player.speed + 5
+                self.getFaster = True
 
+    # Player collects apple
     def collectApple(self):
-        apple = [self.player.head[0], self.player.head[1]]
-        self.player.tail.append(apple)
-        self.spawnApple()
+        apple = [self.player.head[0], self.player.head[1]]  # location of the apple
+        self.player.tail.append(apple)  # add one element to the tail
+        self.player.score = self.player.score + 5  # increase score
+        self.spawnApple()  # spawn a new apple
 
+    # Did the Player hit anything?
     def checkCollision(self):
-        temp = self.player.head.copy()
+        temp = self.player.head.copy()  # copy current pos
         if self.player.head[0] <= 0 or self.player.head[0] >= self.x - 1 or self.player.head[1] <= 0 or \
-                self.player.head[1] >= self.y - 1:
-            if self.border:
+                self.player.head[1] >= self.y - 1:  # did he hit the boarder?
+            if self.border:  # if border rule on -> Dead
                 self.player.curDirection = "None"
                 self.player.alive = False
-            else:
+            else:  # if border rule off -> show up on the other site
                 if self.player.head[0] <= 0:
                     self.player.head[0] = self.y - 1
                 elif self.player.head[0] >= self.x - 1:
@@ -54,23 +65,24 @@ class Game(object):
                     self.player.head[1] = self.x - 1
                 elif self.player.head[1] >= self.y - 1:
                     self.player.head[1] = 0
-        elif self.player.head[0] == self.apple[0] and self.player.head[1] == self.apple[1]:
+        elif self.player.head[0] == self.apple[0] and self.player.head[1] == self.apple[1]:  # did he hit the apple?
             self.collectApple()
-        elif temp in self.player.tail:
+        elif temp in self.player.tail:  # did he hit the tail?
             self.player.curDirection = "None"
             self.player.alive = False
 
 
-
 class Player(object):
     def __init__(self, x: int, y: int, speed: int):
+        # setup player
         self.head = [x, y]
         self.tail = []
         self.speed = speed
         self.curDirection = "None"
         self.alive = True
+        self.score = 0
 
-    def move(self, direction):
+    def move(self, direction):  # someone hit a key
         if direction == "UP" and self.curDirection != "DOWN":
             self.curDirection = "UP"
         elif direction == "DOWN" and self.curDirection != "UP":
@@ -80,15 +92,15 @@ class Player(object):
         elif direction == "RIGHT" and self.curDirection != "LEFT":
             self.curDirection = "RIGHT"
 
-    def moveTail(self):
+    def moveTail(self):  # tail is moving
         for i in range(0, len(self.tail)):
             temp = self.tail.copy()
-            if i == len(self.tail)-1:
+            if i == len(self.tail) - 1:
                 self.tail[i] = [self.head[0], self.head[1]]
             else:
-                self.tail[i] = temp[i+1]
+                self.tail[i] = temp[i + 1]
 
-    def autoMove(self):
+    def autoMove(self):  # also moving if nobody hit a key
         if self.curDirection != "None":
             self.moveTail()
             if self.curDirection == "UP":
@@ -101,14 +113,68 @@ class Player(object):
                 self.head[0] = self.head[0] + 1
 
 
-class Window(QWidget, object):
+class Window(QWidget, object):  # gamewindow
     def __init__(self, game: Game):
         super().__init__()
+
+        # timer for repeat
         self.timer = QTimer()
+
+        # Gameinfo
         self.game = game
         self.player = game.player
-        self.initMe()
+        self.highscore = [0, 0, 0]
 
+        # Outer win settings
+        self.setGeometry(10, 30, 100, 100)
+        self.setWindowTitle("Snake")
+
+        self.form = QFormLayout()  # Layout
+
+        # creat board
+        width, hight = 500, 500
+
+        board = QtGui.QPixmap(50, 50)
+        board.fill(QColor(100, 100, 255))
+
+        img = board.toImage()
+
+        # Screen
+        self.display = QLabel()
+
+        # Statusbar
+        self.statusbar = QStatusBar()
+        self.statusbar.setObjectName("statusbar")
+        self.displayScore = QLabel("Score: 0")
+        self.statusbar.addWidget(self.displayScore)
+
+        # Menuebar
+        self.menubar = QMenuBar(self)
+        self.menubar.setObjectName("menubar")
+        self.menu = QMenu("Pause")
+        self.menubar.addMenu(self.menu)
+        self.menu.addAction("Pause", self.pauseGame)
+        self.menubar.move(0, 0)
+
+        # Highscore
+        self.displayHighScore = QLabel("Highscore: 0 / 0 / 0")
+
+        # Widget und Layout hinzufügen
+        self.form.addWidget(self.display)
+        self.form.addWidget(self.statusbar)
+        self.form.addWidget(self.menubar)
+        self.form.addWidget(self.displayHighScore)
+
+        self.setLayout(self.form)
+
+        # timer
+        self.checkSettings()
+
+    # pause game
+    def pauseGame(self):
+        self.player.curDirection = "None"
+
+    # call in loop
     def onRepeat(self):
         # erstellen des Speilfeldes
         width, hight = self.game.x * 10, self.game.y * 10
@@ -151,45 +217,54 @@ class Window(QWidget, object):
 
         scaledBoardImg = img.scaled(width, hight, Qt.AspectRatioMode.KeepAspectRatio)
 
+        # show map and score
         self.display.setPixmap(QPixmap.fromImage(scaledBoardImg))
+        self.displayScore.setText("Score: " + str(self.player.score))
 
+        # check if the time has to be updated
         if self.game.getFaster:
             self.game.getFaster = False
             self.checkSettings()
+
+        # check if the game ist over
+        self.checkGameEnd()
+
+    # Check function
+    def checkGameEnd(self):
+        if not self.player.alive:
+            # msg box
+            msg = QMessageBox()
+            msg.setWindowTitle("GAME OVER")
+            msg.setText("Score: " + str(self.player.score))
+
+            self.timer.stop()
+
+            x = msg.exec()
+
+            # safe highscore
+            self.highscore.append(self.player.score)
+            self.highscore.sort(reverse=True)
+            self.highscore.pop()
+
+            # start new game
+            self.game = Game(self.game.x, self.game.y)
+            self.player = self.game.player
+            self.timer.start()
+
+            # update highscore
+            self.displayHighScore.setText(
+                "Highscore: " + str(self.highscore[0]) + " / " + str(self.highscore[1]) + " / " + str(
+                    self.highscore[2]))
 
     def checkSettings(self):
         # timer
         self.timer = QTimer()
         self.timer.setSingleShot(False)
-        self.timer.setInterval(150 - self.game.player.speed)  # in milliseconds, so 5000 = 5 seconds
+        self.timer.setInterval(150 - self.game.player.speed)                # in milliseconds and controls the speed
         self.timer.timeout.connect(self.onRepeat)
         self.timer.start()
 
-
-    def initMe(self):
-        self.setGeometry(10, 30, 100, 100)  # Position: (10,30) auf dem Bildschirm Größe:  (300,200)
-        self.setWindowTitle("Snake")  # Titel setzen
-
-        # in QLabel wird das Bild angezeigt
-        self.display = QLabel()
-        form = QFormLayout()
-
-        # erstellen des Speilfeldes
-        width, hight = 500, 500
-
-        board = QtGui.QPixmap(50, 50)
-        board.fill(QColor(100, 100, 255))
-
-        img = board.toImage()
-
-        # Widget und Layout hinzufügen
-        form.addWidget(self.display)
-        self.setLayout(form)
-        self.show()
-
-        # timer
-        self.checkSettings()
-
+    # this function is called when a key is pressed
     def keyPressEvent(self, QKeyEvent):
         if QKeyEvent.key() == Qt.Key.Key_Up:
             self.player.move("UP")
@@ -201,11 +276,12 @@ class Window(QWidget, object):
             self.player.move("RIGHT")
 
 
+# Settings
 class Ui_MainWindow(object):
-    def __init__(self, win: Window):
+    def __init__(self, winIn: Window):
         super().__init__()
         self.timer = QTimer()
-        self.game = win
+        self.game = winIn
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -215,41 +291,38 @@ class Ui_MainWindow(object):
         self.checkBoxBorder = QCheckBox(parent=self.centralwidget)
         self.checkBoxBorder.setGeometry(QRect(30, 10, 86, 20))
         self.checkBoxBorder.setObjectName("checkBoxBorder")
+        self.checkBoxBorder.setToolTip("Border hit = game over")
 
-        #button
-        self.submitSettings = QPushButton(parent=self.centralwidget, clicked= self.submit) #Button submit settings
+        # button
+        self.submitSettings = QPushButton(parent=self.centralwidget, clicked=self.submit)  # Button submit settings
         self.submitSettings.setGeometry(QRect(20, 130, 113, 32))
         self.submitSettings.setObjectName("submitSettings")
+        self.submitSettings.setToolTip("Press this Button to Submit your Settings and start the game")
 
-        #Slider
+        # Slider
         self.SlideSpeed = QSlider(parent=self.centralwidget)
         self.SlideSpeed.setGeometry(QRect(90, 40, 160, 22))
         self.SlideSpeed.setOrientation(Qt.Orientation.Horizontal)
         self.SlideSpeed.setObjectName("SlideSpeed")
         self.SlideSpeed.valueChanged.connect(self.speedSlider)
+        self.SlideSpeed.setToolTip("You can chose the speed of your character")
 
         self.label = QLabel(parent=self.centralwidget)
         self.label.setGeometry(QRect(30, 40, 60, 16))
         self.label.setObjectName("label")
+
         self.checkBoxFaster = QCheckBox(parent=self.centralwidget)
         self.checkBoxFaster.setGeometry(QRect(30, 70, 161, 20))
         self.checkBoxFaster.setObjectName("checkBoxFaster")
+        self.checkBoxFaster.setToolTip("every Apple makes you faster")
+
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(parent=MainWindow)
-        self.menubar.setGeometry(QRect(0, 0, 343, 24))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(parent=MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
-
-        #default val
+        # default val
         if self.game.game.border:
             self.checkBoxBorder.setChecked(True)
         if self.game.game.speedUp:
             self.checkBoxFaster.setChecked(True)
-
 
         self.retranslateUi(MainWindow)
         QMetaObject.connectSlotsByName(MainWindow)
@@ -269,6 +342,8 @@ class Ui_MainWindow(object):
             self.game.game.speedUp = False
 
         self.game.checkSettings()
+        self.game.show()
+        MainWindow.close()
 
     def retranslateUi(self, MainWindow):
         _translate = QCoreApplication.translate
@@ -279,13 +354,12 @@ class Ui_MainWindow(object):
         self.checkBoxFaster.setText(_translate("MainWindow", "becoming faster"))
 
 
-
 app = QApplication(sys.argv)
 
-#game
-win = Window(Game(60, 60))
+# game
+win = Window(Game(30, 30))
 
-#settings
+# settings
 MainWindow = QMainWindow()
 ui = Ui_MainWindow(win)
 ui.setupUi(MainWindow)
