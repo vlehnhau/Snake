cc2f24cb58aa3121e8d90c325ab49a4f772856bc
malvesbr <malvesbr@students.uni-mainz.de>
2023-04-29 16:13:52 +0200

    Main Menu Works now

--- main.py
+++ main.py
@@ -5,41 +5,6 @@ from PyQt5 import QtWidgets as qw
 from PyQt5 import QtGui as qg
 from PyQt5 import QtCore as qc
 
-class MainWindow(qw.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Main Menu")
-        self.setGeometry(100, 100, 300, 150)
-
-        # Border CheckBox
-        borderCB = qw.QCheckBox()
-        borderCB.setChecked(False)
-        borderCB.setToolTip("Checked: Die when hitting the border")
-
-        # Speed Slider
-        speedSL = qw.QSlider(qc.Qt.Horizontal)
-        speedSL.setMinimum(1)
-        speedSL.setMaximum(10)
-        speedSL.setValue(5)
-        speedSL.setToolTip("Decide how fast the game is")
-
-        # Fruits Slider
-        fruitsSL = qw.QSlider(qc.Qt.Horizontal)
-        fruitsSL.setMinimum(1)
-        fruitsSL.setMaximum(10)
-        fruitsSL.setValue(5)
-        fruitsSL.setToolTip("Decide how often fruits spawn")
-
-        startBU = qw.QPushButton("Start")
-        startBU.setToolTip("Start the game with your settings")
-
-        # Layout of Main Menu
-        layout = qw.QFormLayout()
-        layout.addRow(qw.QLabel("border:"), borderCB)
-        layout.addRow(qw.QLabel("speed: "), speedSL)
-        layout.addRow(qw.QLabel("fruits: "), fruitsSL)
-        layout.addRow(startBU)
-        self.setLayout(layout)
 
 class Snake(qw.QWidget):
     def __init__(self):
@@ -51,8 +16,11 @@ class Snake(qw.QWidget):
         self.lastDir = None
 
         self.borders = False
+        self.speed = 100
 
+        self.fruitsSR = 10  # fruits Spawn-Rate
         self.fruits = []
+
         #Bild von Spiel
         game = qw.QLabel(self)
         self.pic = qg.QImage(32,32, qg.QImage.Format_RGBA8888)
@@ -69,14 +37,16 @@ class Snake(qw.QWidget):
 
         # Timer
         self.timer = qc.QTimer()
-        self.timer.setInterval(1000)  # Timer-Reset alle 300ms
         self.timer.timeout.connect(self.timerFunction)
-        self.timer.start(300)
+
 
         scaledPic = self.pic.scaled(500, 500)
         game.setPixmap(qg.QPixmap.fromImage(scaledPic))
     def keyPressEvent(self, e):
-        if self.moving == False: self.moving = True
+        if self.moving == False:
+            self.moving = True
+            self.timer.setInterval(self.speed)
+            self.timer.start()
         if e.key() == qc.Qt.Key.Key_Up:
             if self.lastDir != "Down":
                 self.lastDir = "Up"
@@ -145,7 +115,7 @@ class Snake(qw.QWidget):
             game.setPixmap(qg.QPixmap.fromImage(scaledPic))
 
     def spawnFruit(self):
-        rndNum = random.randint(0,11)
+        rndNum = random.randint(0,self.fruitsSR)
         if rndNum == 0:
             rndX = random.randint(0,32)
             rndY = random.randint(0,32)
@@ -153,25 +123,79 @@ class Snake(qw.QWidget):
                 rndX = random.randint(0, 32)
                 rndY = random.randint(0, 32)
 
-            self.fruits.append((rndX,rndY))
+            self.fruits.append((rndX, rndY))
+
+class MainWindow(qw.QWidget):
+
+    def __init__(self, winIn: Snake):
+        super().__init__()
+        self.setWindowTitle("Main Menu")
+        self.setGeometry(100, 100, 300, 150)
+        self.game = winIn
+        self.snake_game = Snake()
+
+
+        # Border CheckBox
+        self.borderCB = qw.QCheckBox()
+        self.borderCB.setChecked(False)
+        self.borderCB.setToolTip("Check: Die when hitting the border")
+
+        def borderCBTooltip(state):
+            if state == qc.Qt.Checked:
+                self.borderCB.setToolTip("Uncheck: Allows the snake to continue on the opposite side of the playing field when hitting the border.")
+            else:
+                self.borderCB.setToolTip("Check: Lose when hitting the border")
+        self.borderCB.stateChanged.connect(borderCBTooltip)
+
+        # Speed Slider
+        self.speedSL = qw.QSlider(qc.Qt.Horizontal)
+        self.speedSL.setMinimum(1)
+        self.speedSL.setMaximum(10)
+        self.speedSL.setValue(5)
+        self.speedSL.setToolTip("Decide how fast the game is")
+
+        # Fruits Slider
+        self.fruitsSL = qw.QSlider(qc.Qt.Horizontal)
+        self.fruitsSL.setMinimum(1)
+        self.fruitsSL.setMaximum(10)
+        self.fruitsSL.setValue(5)
+        self.fruitsSL.setToolTip("Decide how often fruits spawn")
+
+        # Button
+        startBU = qw.QPushButton("Start", self)
+        startBU.setToolTip("Start the game with your settings")
+        startBU.clicked.connect(self.startSnake)
+
+        # Layout of Main Menu
+        layout = qw.QFormLayout()
+        layout.addRow(qw.QLabel("border:"), self.borderCB)
+        layout.addRow(qw.QLabel("speed: "), self.speedSL)
+        layout.addRow(qw.QLabel("fruits: "), self.fruitsSL)
+        layout.addRow(startBU)
+        self.setLayout(layout)
+
+        # self.game.borders = True
+
+    def startSnake(self):
+        self.snakeGame = Snake()
+        self.snakeGame.borders = self.borderCB.isChecked()
+        self.snakeGame.speed = 125 - (10 * self.speedSL.value())
+        self.snakeGame.fruitsSR = 16 - self.fruitsSL.value()
+        self.snakeGame.show()
+        self.close()
+
+
 
 if __name__=="__main__":
     app = qw.QApplication(sys.argv)
-    win = MainWindow()
-    #win = Snake()
+    game = Snake()
+    win = MainWindow(game)
     win.show()
     sys.exit(app.exec_())
 
 
-# Bugs die noch gefixt werden müssen:
-# 1. Rückwärts laufen beim schnellen Richtungswechsel
-
 # To do:
-# Main Menü ans Spiel anknüpfen (Beim klick auf "Start" öffnet sich das Spiel)
-# Fruit Spawn Rait
-# Speed
-# Border (Funktionalität schon umgesetzt
-# Tooltips
 # Menüleiste (Pausieren, beenden, Punkte)
 # defead Funktion (Endpunktestart restart?)
 # Highscore der vergangenen Spiele
+# Qt Creator
