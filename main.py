477687405b319d5b957a0158ba214cdb3f3cb283
malvesbr <malvesbr@students.uni-mainz.de>
2023-04-30 11:29:25 +0200

    Version 1.0

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Snake(qw.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Snake")
-        self.setGeometry(100,100,500,500)
+        self.setGeometry(100,100,500,550)
 
         self.moving = False
         self.lastDir = None
@@ -42,23 +42,43 @@ class Snake(qw.QWidget):
 
         scaledPic = self.pic.scaled(500, 500)
         game.setPixmap(qg.QPixmap.fromImage(scaledPic))
+
+        # Pause Button
+        self.paused = False
+        self.pauseBU = qw.QPushButton("PAUSE", self)
+        self.pauseBU.move(10,510)
+        self.pauseBU.clicked.connect(self.pauseGame)
+        self.setFocus()
+
+        # Score Text
+        self.score = qw.QLabel("SCORE: 0", self)
+        self.score.move(120,516)
+
+        self.highs = []
+        self.highscoreText = qw.QLabel("HIGHSCORES:", self)
+        self.highscoreText.move(200, 516)
+        self.highscores = qw.QLabel("[                     ]", self)
+        self.highscores.move(285, 516)
+
+
     def keyPressEvent(self, e):
-        if self.moving == False:
-            self.moving = True
-            self.timer.setInterval(self.speed)
-            self.timer.start()
-        if e.key() == qc.Qt.Key.Key_Up:
-            if self.lastDir != "Down":
-                self.lastDir = "Up"
-        if e.key() == qc.Qt.Key.Key_Down:
-            if self.lastDir != "Up":
-                self.lastDir = "Down"
-        if e.key() == qc.Qt.Key.Key_Left:
-            if self.lastDir != "Right":
-                self.lastDir = "Left"
-        if e.key() == qc.Qt.Key.Key_Right:
-            if self.lastDir != "Left":
-                self.lastDir = "Right"
+        if not self.paused:
+            if self.moving == False:
+                self.moving = True
+                self.timer.setInterval(self.speed)
+                self.timer.start()
+            if e.key() == qc.Qt.Key.Key_Up:
+                if self.lastDir != "Down":
+                    self.lastDir = "Up"
+            if e.key() == qc.Qt.Key.Key_Down:
+                if self.lastDir != "Up":
+                    self.lastDir = "Down"
+            if e.key() == qc.Qt.Key.Key_Left:
+                if self.lastDir != "Right":
+                    self.lastDir = "Left"
+            if e.key() == qc.Qt.Key.Key_Right:
+                if self.lastDir != "Left":
+                    self.lastDir = "Right"
 
     def timerFunction(self):
         if self.moving == True:
@@ -74,8 +94,7 @@ class Snake(qw.QWidget):
 
             if self.borders:
                 if self.headX < 0 or self.headX > 31 or self.headY < 0 or self.headY > 31:
-                    self.timer.stop()
-                    # add Function for ending Game
+                    self.endGame()
             else:
                 if self.headX < 0:
                     self.headX = 31
@@ -88,14 +107,14 @@ class Snake(qw.QWidget):
 
 
             if ((self.headX, self.headY) in self.snakeLoc):
-                self.timer.stop()
-                # add Function for ending Game
+                self.endGame()
 
             self.snakeLoc.insert(0, (self.headX, self.headY))
 
             if self.snakeLoc[0] in self.fruits:
                 self.snakeLen += 1
                 self.fruits.remove(self.snakeLoc[0])
+                self.score.setText("SCORE: " + str(self.snakeLen - 1))
 
             if len(self.snakeLoc) > self.snakeLen:
                 self.snakeLoc.pop()
@@ -124,6 +143,73 @@ class Snake(qw.QWidget):
                 rndY = random.randint(0, 32)
 
             self.fruits.append((rndX, rndY))
+    def pauseGame(self):
+        if self.paused:
+            self.timer.start()
+            self.paused = False
+        else:
+            self.timer.stop()
+            self.paused = True
+        self.setFocus()
+
+    def endGame(self):
+        self.timer.stop()
+        if len(self.highs) == 0:
+            self.highs.insert(0, self.snakeLen - 1)
+        elif self.snakeLen - 1 >= self.highs[0]:
+            self.highs.insert(0, self.snakeLen - 1)
+            if len(self.highs) > 3: self.highs.pop()
+        elif self.snakeLen - 1 >= self.highs[1]:
+            self.highs.insert(1, self.snakeLen - 1)
+            if len(self.highs) > 3: self.highs.pop()
+        elif self.snakeLen - 1 >= self.highs[2]:
+            self.highs.insert(2, self.snakeLen - 1)
+            if len(self.highs) > 3: self.highs.pop()
+
+        self.endGamelayout = qw.QFormLayout()
+        self.endWindow = qw.QWidget()
+        self.endWindow.setWindowTitle("GAME OVER!")
+        self.endWindow.setGeometry(150, 150, 200, 200)
+
+        self.endGamelayout.addRow(qw.QLabel("GAME OVER!"))
+        self.endGamelayout.addRow(qw.QLabel(""))
+        self.endGamelayout.addRow(qw.QLabel("Score: " + str(self.snakeLen - 1)))
+        self.endGamelayout.addRow(qw.QLabel(""))
+        self.endGamelayout.addRow(qw.QLabel("Highscores:"))
+        self.endGamelayout.addRow(qw.QLabel(str(self.highs[0])))
+        if len(self.highs) >= 2:
+            self.endGamelayout.addRow(qw.QLabel(str(self.highs[1])))
+        if len(self.highs) == 3:
+            self.endGamelayout.addRow(qw.QLabel(str(self.highs[2])))
+
+        self.restartBU = qw.QPushButton("Restart")
+        self.restartBU.clicked.connect(self.restart)
+
+        self.endGamelayout.addRow(self.restartBU)
+
+
+        self.endWindow.setLayout(self.endGamelayout)
+        self.endWindow.show()
+        print(self.highs)
+
+    def restart(self):
+        self.timer.stop()
+        self.endWindow.close()
+        self.headX = 10
+        self.headY = 10
+        self.snakeLen = 1
+        self.snakeLoc = [(self.headX, self.headY)]
+        self.lastDir = None
+        self.fruits = []
+        self.score.setText("SCORE: 0")
+        self.paused = False
+        self.moving = False
+        self.pic.fill(qc.Qt.gray)
+        self.pic.setPixelColor(self.headX, self.headY, qc.Qt.green)
+        self.highscores.setText(str(self.highs))
+        scaledPic = self.pic.scaled(500, 500)
+        game = self.findChild(qw.QLabel)
+        game.setPixmap(qg.QPixmap.fromImage(scaledPic))
 
 class MainWindow(qw.QWidget):
 
